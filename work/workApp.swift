58476b812ee107ced535e5f6ead20acad8ffac6b
Rayna Shah <raynaemail@gmail.com>
2022-11-20 19:24:27 -0600

    Initial Commit

---
+++ work/workApp.swift
@@ -0,0 +1,17 @@
+//
+//  workApp.swift
+//  work
+//
+//  Created by Rayna Shah on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
